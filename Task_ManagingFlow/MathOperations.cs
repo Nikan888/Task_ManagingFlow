6d8a8bafe3caea2228a1de9eec82255d15ecbfce
Nikan <32056409+Nikan888@users.noreply.github.com>
2018-07-23 14:56:45 +0300

    Task_ManagingFlow

--- Task_ManagingFlow/MathOperations.cs
+++ Task_ManagingFlow/MathOperations.cs
@@ -8,6 +8,76 @@ namespace Task_ManagingFlow
 {
     public class MathOperations
     {
+        public static double FindVariationRange(double min, double max)
+        {
+            return max - min;
+        }
 
+        public static double FindLengthInterval(double range, int numberOfIntervals)
+        {
+            return range / numberOfIntervals;
+        }
+
+        public static double[] FormingPartialIntervals(double[] array, int numberOfIntervals)
+        {
+            double range = FindVariationRange(array.Min(), array.Max());
+            double[] intervals = new double[numberOfIntervals + 1];
+            intervals[0] = array.Min();
+            for (int i = 1; i < numberOfIntervals + 1; i++)
+            {
+                intervals[i] = intervals[i - 1] + FindLengthInterval(range, numberOfIntervals);
+            }
+            return intervals;
+        }
+
+        public static double[] FindMidpointsOfIntervals(double[] intervals)
+        {
+            double[] midpoints = new double[intervals.Length - 1];
+            for (int i = 0; i < intervals.Length - 1; i++)
+            {
+                midpoints[i] = (intervals[i] + intervals[i + 1]) / 2;
+            }
+            return midpoints;
+        }
+
+        public static double[] FindIntervalFrequency(double[] array, double[] intervals)
+        {
+            Array.Sort(array);
+            double[] frequencies = new double[intervals.Length - 1];
+            int i = 0, j = 0;
+            do
+            {
+                labelA:
+                if (j < array.Length && array[j] >= intervals[i] && array[j] <= intervals[i + 1])
+                {
+                    frequencies[i]++;
+                    j++;
+                    goto labelA;
+                }
+                else i++;
+
+            } while (i < intervals.Length - 1);
+            return frequencies;
+        }
+
+        public static double[] FindRelativeFrequency(double[] frequencies)
+        {
+            double[] relatives = new double[frequencies.Length];
+            for (int i = 0; i < frequencies.Length; i++)
+            {
+                relatives[i] = frequencies[i] / 100;
+            }
+            return relatives;
+        }
+
+        public static double[] FindDistributionDensity(double[] relatives, double length)
+        {
+            double[] density = new double[relatives.Length];
+            for (int i = 0; i < relatives.Length; i++)
+            {
+                density[i] = relatives[i] / length;
+            }
+            return density;
+        }
     }
 }
--- Task_ManagingFlow/Program.cs
+++ Task_ManagingFlow/Program.cs
@@ -10,6 +10,170 @@ namespace Task_ManagingFlow
     {
         static void Main(string[] args)
         {
+            try
+            {
+                //Console.WriteLine("Input numbers count");  //Manual input
+                //int n = CheckOnNegativity();
+                //double[] array = new double[n];
+                //for (int i = 0; i < n; i++)
+                //{
+                //    array[i] = Convert.ToDouble(Console.ReadLine());
+                //}
+
+                double[] array = { 16.8, 17.9, 21.4, 14.1, 19.1, 18.1, 15.1, 18.2, 20.3, 16.7 };
+                int numberOfIntervals = 3;
+                bool exit = false;
+
+                while (!exit)
+                {
+
+                    Console.Clear();
+                    Console.WriteLine("Menu:");
+                    Console.WriteLine("1. Sort set of numbers");
+                    Console.WriteLine("2. Find range of variation");
+                    Console.WriteLine("3. Output lower bounds of partial intervals");
+                    Console.WriteLine("4. Find midpoints of partial intervals");
+                    Console.WriteLine("5. Find interval frequency");
+                    Console.WriteLine("6. Find relative frequency of intervals");
+                    Console.WriteLine("7. Find distribution density");
+                    Console.WriteLine("0. Exit");
+                    int select = Convert.ToInt32(Console.ReadLine());
+                    Console.Clear();
+                    switch (select)
+                    {
+                        case 1:
+                            {
+                                Array.Sort(array);
+                                Console.WriteLine("Sorted set: ");
+                                foreach (var s in array)
+                                {
+                                    Console.Write(s + " ");
+                                }
+                                Console.WriteLine();
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.WriteLine("Range of variation: {0}", MathOperations.FindVariationRange(array.Min(), array.Max()));
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 3:
+                            {
+                                double[] intervals = MathOperations.FormingPartialIntervals(array, numberOfIntervals);
+                                Console.WriteLine("Lower bounds of partial intervals: ");
+                                for (int i = 0; i < intervals.Length - 1; i++)
+                                {
+                                    Console.WriteLine("{0:0.#}", intervals[i]);
+                                }
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 4:
+                            {
+                                double[] intervals = MathOperations.FormingPartialIntervals(array, numberOfIntervals);
+
+                                double[] midpoints = MathOperations.FindMidpointsOfIntervals(intervals);
+                                Console.WriteLine("Midpoints of intervals: ");
+                                foreach (var m in midpoints)
+                                {
+                                    Console.WriteLine("{0:0.#}", m);
+                                }
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 5:
+                            {
+                                double[] intervals = MathOperations.FormingPartialIntervals(array, numberOfIntervals);
+
+                                double[] frequencies = MathOperations.FindIntervalFrequency(array, intervals);
+                                Console.WriteLine("Interval frequencies: ");
+                                foreach (var f in frequencies)
+                                {
+                                    Console.WriteLine(f);
+                                }
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 6:
+                            {
+                                double[] intervals = MathOperations.FormingPartialIntervals(array, numberOfIntervals);
+                                double[] frequencies = MathOperations.FindIntervalFrequency(array, intervals);
+
+                                double[] relatives = MathOperations.FindRelativeFrequency(frequencies);
+                                Console.WriteLine("Relative frequencies: ");
+                                foreach (var r in relatives)
+                                {
+                                    Console.WriteLine(r);
+                                }
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 7:
+                            {
+                                double range = MathOperations.FindVariationRange(array.Min(), array.Max());
+                                double length = MathOperations.FindLengthInterval(range, numberOfIntervals);
+                                double[] intervals = MathOperations.FormingPartialIntervals(array, numberOfIntervals);
+                                double[] frequencies = MathOperations.FindIntervalFrequency(array, intervals);
+                                double[] relatives = MathOperations.FindRelativeFrequency(frequencies);
+
+                                double[] density = MathOperations.FindDistributionDensity(relatives, length);
+                                Console.WriteLine("Distribution density: ");
+                                foreach (var d in density)
+                                {
+                                    Console.WriteLine(d);
+                                }
+                                Console.WriteLine("Press any button to contunue");
+                                Console.ReadKey();
+                                break;
+                            }
+                        case 0:
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Thank u for using this software. Good luck!");
+                                Console.WriteLine("Press any button to exit");
+                                Console.ReadKey();
+                                exit = true;
+                                break;
+                            }
+                        default: break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();
+        }
+
+        static int CheckOnNegativity()
+        {
+            int i = 0;
+            do
+            {
+                var input = Console.ReadLine();
+                try
+                {
+                    int.TryParse(input, out i);
+                    if (i <= 0)
+                    {
+                        Console.WriteLine("Value must be more than 0");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Value must be Int32");
+                }
+            } while (i <= 0);
+            return i;
         }
     }
 }
